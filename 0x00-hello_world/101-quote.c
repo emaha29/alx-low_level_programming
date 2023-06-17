33ac3d275cbd8aab937c284e510adcc56bfcf913
emaha29 <maha.hassan955@yahoo.com>
2023-06-17 12:53:56 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
 */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Ropar, 2015-10-19\n";
-write(1, quo, 59);
+char quo[] =("and that piece of art is useful\" - Dora Ropar, 2015-10-19\n", write, 1, quo, 59);
 return (1);
 }
