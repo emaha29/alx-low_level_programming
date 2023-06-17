36ec9e76774795372ac1b169f4a36c6fba89b9a7
emaha29 <maha.hassan955@yahoo.com>
2023-06-17 09:54:19 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main-Entry Point
+*
+* Description: Program that Printf that Art.
+* Return: always 0(Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
