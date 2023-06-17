c454aafedfdc9ff76f04744b5d037e20a2600980
emaha29 <maha.hassan955@yahoo.com>
2023-06-17 09:04:56 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
+/**
+* main - entry Point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+pust("\"Programming is like building a multilingual puzzle");
 return (0);
 }
