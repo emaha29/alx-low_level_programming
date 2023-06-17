8cd4270ed59705d3d44c9516854201cf0aa8e3d0
emaha29 <maha.hassan955@yahoo.com>
2023-06-17 12:14:38 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main-Entry point
+*
+* Description:print a quote using write Function
+* write:int fd:void siz count
+* Return:1(Not Success)
+*/
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Ropar, 2015-10-19\n";
+write(1, quo, 59);
+return (1);
+}
