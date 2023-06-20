185d4ab7aab577ef7406cb450b4642075f64ff1f
emaha29 <maha.hassan955@yahoo.com>
2023-06-20 05:19:48 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+_putchar(43);
 return (1);
 }
 else if (n == 0)
@@ -18,9 +18,9 @@ else if (n == 0)
 _putchar(48);
 return (0);
 }
-else if (n < 0)
+else
 {
-_putchar('-');
+_putchar(45);
 }
 return (-1);
 }
